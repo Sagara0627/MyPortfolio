78e22a2c5a34e27d3df05b08dd49001ccdf4d2a2
相良 征哉 <sagara-yuya@meijitosho.co.jp>
2025-09-05 07:36:59 +0900

    gh-pages Github Pagesではサブディレクトリで公開されるための考慮

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/MyPortfolio/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
